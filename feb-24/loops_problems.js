be5b9272b65389d1fc511493059a3d2a70ec7039
TatevG <tatevgalstian@gmail.com>
2018-03-03 23:20:51 +0400

    Loop problems solutions

--- feb-24/loops_problems.js
+++ feb-24/loops_problems.js
@@ -1,11 +1,11 @@
 // factorial
 
-let n = ;
-let result = ;
+let n = 5;
+let result = 1;
 
-/*
-  your code
-*/
+for (let i = 1; i <= n; i++) {
+  result *= i;
+}
 
 console.log(result); //prints the factorial of n
 
@@ -13,12 +13,15 @@ console.log(result); //prints the factorial of n
 
 // sum of odd numbers
 
-let x = ;
-let sum = ;
+let x = 15;
+let sum = 0;
 
-/*
-  your code
-*/
+for (let i = 0; i < x; i++) {
+  if (i%2 ==0) {
+    sum +=i;
+  }
+  
+}
 
 console.log(sum); //print the sum of odd numbers in range 0 to x
 
@@ -26,24 +29,25 @@ console.log(sum); //print the sum of odd numbers in range 0 to x
 
 // number of digits
 
-let number = ;
-let numberOfDigits = ;
+let number = 485;
+let numberOfDigits = 0;
 
-/*
-  your code
-*/
-
+while(number != 0){
+  numberOfDigits++;
+  number = parseInt(number/10);
+}
 console.log(numberOfDigits); // print the number of digits of number
 
 
 
 // product of digits
 
-let num = ;
-let product = ;
+let num = 954;
+let product = 1;
 
-/*
-  your code
-*/
+while (num != 0) {
+  product *= (num%10);
+  num = parseInt(num/10);
+}
 
 console.log(product); //prints the product of digits of num
