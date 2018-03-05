2ab02ec02b30686ae74f676fd71abf42a2919113
TatevG <tatevgalstian@gmail.com>
2018-03-05 11:40:55 +0400

    Making classes

--- mar-3/classes.js
+++ mar-3/classes.js
@@ -1,6 +1,27 @@
 // make a class
 // make another class that extends the first class
 // call the contructor of the first class from the second class
-// add a function to the second class
+// add a function to the second class 
 // make an object out of first class
 // call a function of the object
+class Shapes {
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    getArea(){
+        console.log(`Width is ${this.width}, height is ${this.height} => area is ${this.width * this.height}`);
+    }
+}
+class Rectangle extends Shapes{
+    constructor(width, height) {
+        super(width, height);
+    }
+    getPerimeter(){
+        console.log(`Pertimeter of rectangle is equal to ${2*(this.width + this.height)}`);
+    }
+}
+
+const rect = new Rectangle(12, 3);
+rect.getArea();
+rect.getPerimeter();
