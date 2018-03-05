424abff5ba330b171ed239f2f13304c37f8cdfee
TatevG <tatevgalstian@gmail.com>
2018-03-05 12:02:37 +0400

    Hoisting

--- mar-3/hoisting.js
+++ mar-3/hoisting.js
@@ -1,21 +1,26 @@
 // run the program
 // see that it is throwing exceptions
-// DO NOT REORDER the lines in the program only modify them
-// make the program run without errors
-// dont care about the result (hint: undefined can be console logged)
+// DO NOT REORDER the lines in the program only modify them +
+// make the program run without errors +
+// dont care about the result (hint: undefined can be console logged) +
 
 
 
 f();
 
-const f = function(){
+// const f = function(){
+//   console.log('I am being hoisted, YAY!');
+// }
+
+function f(){
   console.log('I am being hoisted, YAY!');
 }
 
 
 
 
-
+// console.log(t);
+// let t = 5;
 
 console.log(t);
-let t = 5;
+var t = 5;
