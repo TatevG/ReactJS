745a304a94f59fff5f7f77510aeb4907ca7bc78f
TatevG <tatevgalstian@gmail.com>
2018-03-08 19:17:59 +0400

    Initial commit

--- mar-6/class.js
+++ mar-6/class.js
@@ -3,90 +3,77 @@
 // }
 
 // function f(){
-//   console.log(this.name);
+//   console.log(this.name)
 // }
 
-// const t = f.bind(obj);
-// f();
-// t();
+// const t = f.bind(obj)
+// f()
+// t()
 
-
-// const arr = ['a1','b2','a3',4,5];
+// const arr = ['a1','b2','a3',4,5]
 
 // const f = (arr) => {
-//   const newArr = [...arr];
-// };
+//   const newArr = [...arr]
+// }
 
 // const t = (arr) => {
-//   const newArr = arr.map((element)=>(element));
-// };
+//   const newArr = arr.map((element)=>(element))
+// }
 
-// f(arr);
-// console.log(arr);
-
-
+// f(arr)
+// console.log(arr)
 
 // const obj = {
 //   a: 56
-// };
+// }
 
-// // let a = obj.a;
-// let {a} = obj;
+// // let a = obj.a
+// let {a} = obj
 
-// const newObj = {a, b: 4};
+// const newObj = {a, b: 4}
 
-// console.log(newObj);
-
-
-
-
+// console.log(newObj)
 
 // class A {
 //   static a(){
-//     console.log('hello');
+//     console.log('hello')
 //   }
 
 //   f(){
-//     A.a();
-//     console.log('barev');
+//     A.a()
+//     console.log('barev')
 //   }
 // }
 
-// const o = new A;
-// o.f();
+// const o = new A
+// o.f()
 
-// // console.log(o.a);
-
-
+// // console.log(o.a)
 
 // setTimeout(()=>{
-//   console.log('1');
-// }, 5000);
-
+//   console.log('1')
+// }, 5000)
 
 // const a = setInterval(()=>{
-//   console.log('1');
-// }, 1000);
+//   console.log('1')
+// }, 1000)
 
 // setTimeout(()=>{
-//   clearInterval(a);
-// }, 5000);
+//   clearInterval(a)
+// }, 5000)
 
-
-
-
-const myPromise = new Promise((resolve, rej)=>{
-  setTimeout(()=>{
-    rej('i got rejected');
-  }, 3000);
-});
-
-
-myPromise.then((value)=>{
-  console.log(value);
-}).catch((err)=>{
-  console.log(err);
+const myPromise = new Promise((resolve, rej) => {
+    setTimeout(() => {
+        rej('i got rejected')
+    }, 3000)
 })
 
-console.log('adjasdasd');
+myPromise.then((value) => {
+    console.log(value)
+}).catch((err) => {
+    console.log(err)
+})
+
+console.log('adjasdasd')
+console.log('123123')
 console.log('123123')
