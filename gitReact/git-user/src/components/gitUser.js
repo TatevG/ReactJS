20e661820cc668aee491da44010c63bca81d96fb
TatevG <tatevgalstian@gmail.com>
2018-03-21 23:21:52 +0400

    init commit

--- gitReact/git-user/src/components/gitUser.js
+++ gitReact/git-user/src/components/gitUser.js
@@ -3,26 +3,6 @@ import loading from '../images/loading.gif';
 
 export default 
 class GitUser extends React.Component{
-    // constructor() {
-    //     super();
-    //     console.log('constructor');
-    //     this.state = {
-    //         user: null
-    //     };
-    // }
-    // async componentDidMount() {
-    //     console.log('cdm');
-    //     let user;
-    //     try {
-    //         const result = await fetch('https://api.github.com/users/narghev');
-    //         user = await result.json();
-    //     }
-    //     catch (e) {
-    //         user = null;
-    //     }
-
-    //     this.setState({ user });
-    // }
     render() {
         return (
             <div>
