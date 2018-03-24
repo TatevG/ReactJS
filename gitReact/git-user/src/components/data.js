9c3318a5164ae108eab625d1130327de8ab5ccd4
TatevG <tatevgalstian@gmail.com>
2018-03-24 17:13:08 +0400

    init commit

--- gitReact/git-user/src/components/data.js
+++ gitReact/git-user/src/components/data.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 const Data = ({user}) =>  (
   <div>
     {user ? JSON.stringify(user) : <p>Enter a user name</p>}
@@ -8,11 +7,3 @@ const Data = ({user}) =>  (
 );
 
 export default Data;
-=======
-const Data = ({user}) =>(
-  <div>
-    {user ? JSON.stringify(user) : <p>Enter a username</p>}
-  </div>
-)
-export default Data
->>>>>>> ada2844dac7862f310c6f8e9bd4188a1f03cca25
