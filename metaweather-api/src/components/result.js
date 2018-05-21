d3f92ef7fa38917e089239396dec944c242d9859
TatevG <tatevgalstian@gmail.com>
2018-05-21 16:15:06 +0400

    new changes

--- metaweather-api/src/components/result.js
+++ metaweather-api/src/components/result.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Result = ({ weather, country, city}) =>{
-	// let icon = fetch(`https://www.metaweather.com/static/img/weather/ico/${weather[0].weather_state_abbr}.ico`);
     return (
       <div className="WeatherCard">
 				<h2 className="WeatherCard-city">City:
